791a92e0988c800e3dc7b700185ad5b706818a83
Angeltear <kaloyan.shivarov@gmail.com>
2023-01-08 21:29:28 +0200

    Adding some unit tests. Code cleanup.

--- src/test/java/com/angeltear/microinitiator/MicroInitiatorApplicationTests.java
+++ src/test/java/com/angeltear/microinitiator/MicroInitiatorApplicationTests.java
@@ -2,15 +2,43 @@ package com.angeltear.microinitiator;
 
 import com.angeltear.microinitiator.Config.RedisConfig;
 import io.lettuce.core.RedisClient;
+import jakarta.servlet.ServletContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 class MicroInitiatorApplicationTests {
 
     @Autowired
     RedisConfig redisConfig;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext; //Loads all application beans and controllers into the context
+
+    //Initialize mockMvc object once with @BeforeEach, so we don't have to inside every test
+    @BeforeEach
+    public void setup(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
     @Test
     void contextLoads() {
     }
@@ -20,4 +48,57 @@ class MicroInitiatorApplicationTests {
         RedisClient redisClient = redisConfig.getClient();
         assert redisClient!=null;
     }
+
+    @Test
+    public void whenContextLoadAndProvidesHomeController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        assertNotNull(servletContext);
+        assertTrue(servletContext instanceof MockServletContext);
+        assertNotNull(webApplicationContext.getBean("homeController"));
+    }
+
+    @Test
+    public void postBadRequestForClientId() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/")
+                        .content("{ \"clientId\": 0, \"paymentId\": 1, \"paymentSum\": 1}")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.detailedMessage").value("[ClientID must be greater than 0!]"))
+                .andReturn();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    public void postBadRequestForPaymentId() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/")
+                        .content("{ \"clientId\": 1, \"paymentId\": 0, \"paymentSum\": 1}")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.detailedMessage").value("[Payment ID must be greater than 0!]"))
+                .andReturn();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    public void postBadRequestForPaymentSum() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post("/")
+                        .content("{ \"clientId\": 1, \"paymentId\": 1, \"paymentSum\": 0}")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.detailedMessage").value("[Payment Sum must be greater than 0!]"))
+                .andReturn();
+
+        assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
+    }
+
+
 }
