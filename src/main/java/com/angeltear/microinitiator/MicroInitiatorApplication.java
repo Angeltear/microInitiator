7b79cf0db63ca80ef295124eee0540d48e98758f
Angeltear <kaloyan.shivarov@gmail.com>
2023-01-08 12:43:07 +0200

    Initial commit

---
+++ src/main/java/com/angeltear/microinitiator/MicroInitiatorApplication.java
@@ -0,0 +1,13 @@
+package com.angeltear.microinitiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroInitiatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroInitiatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angeltear/microinitiator/MicroInitiatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angeltear.microinitiator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroInitiatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
